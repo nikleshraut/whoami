80f83b752d6eae28c87c33408f9135885727ffe6
Niklesh Raut <nikleshraut@gmail.com>
2022-04-21 22:32:38 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>

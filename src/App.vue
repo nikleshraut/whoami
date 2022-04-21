bc9deb584996705246c9a4d318ad6b0c15ce14ef
Niklesh Raut <nikleshraut@gmail.com>
2022-04-21 23:38:43 +0530

    Added header button

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import oneTwoThree from './assets/123.jpg'
+//import {row, col} from 'element-plus'
 </script>
 
 <template>
   <div class="common-layout">
     <el-container>
-      <el-header>Header</el-header>
+      <el-header>
+        <el-row :gutter="20">
+          <el-col :xs="6" :span="2"><img class="pointer" :src="oneTwoThree" width="100" height="100" /></el-col>
+          <el-col :xs="6" :span="2"><img class="pointer" :src="oneTwoThree" width="100" height="100" /></el-col>
+          <el-col :xs="6" :span="2"><img class="pointer" :src="oneTwoThree" width="100" height="100" /></el-col>
+          <el-col :xs="6" :span="2"><img class="pointer" :src="oneTwoThree" width="100" height="100" /></el-col>
+        </el-row>
+      </el-header>
       <el-main>Main</el-main>
       <el-footer>Footer</el-footer>
     </el-container>
   </div>
 </template>
+
+<style scoped>
+  .pointer {
+    cursor: pointer;
+  }
+  .pointer:hover {
+    box-shadow: rgba(0, 0, 0, 0.50) 1.95px 1.95px 2.6px;
+  }
+</style>
